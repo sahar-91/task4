a1b6d68906ab6bdeedb2f6620090cad52c681cfd
Sahar Atat <saharatat325@gmail.com>
2026-04-09 13:00:09 +0300

    spacing

--- Components/weWillHelp.js
+++ Components/weWillHelp.js
@@ -2,8 +2,8 @@ export default function WeWillHelp() {
     return (
         <div className="bg-[#fbfbfb]">
             <div className="container mx-auto">
-                <div className="flex md:flex-row flex-col gap-10 mt-6 mb-20 justify-between text-center md:text-start items-center px-8">
-                    <img src="https://www.solidbackgrounds.com/images/950x350/950x350-gray-solid-color-background.jpg" alt="image" className="w-100 h-75 rounded-lg object-cover" />
+                <div className="flex md:flex-row flex-col gap-10 mt-6 mb-20 justify-between text-center md:text-start items-center p-8">
+                    <img src="https://www.solidbackgrounds.com/images/950x350/950x350-gray-solid-color-background.jpg" alt="image" className="md:w-[504px] md:h-[370px] h-[210px] rounded-lg object-cover" />
                     <div className="w-full">
                         <h2 className="text-2xl font-bold mb-4">We will help you become an employee in your dream company</h2>
                         <p className="text-[#8A8A8A] mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-        <body className="min-h-full flex flex-col">
+        <body className="bg-[#fbfbfb] min-h-full flex flex-col">
         <Navbar />
         <Banner />
         <main className="flex-1">{children}</main>
